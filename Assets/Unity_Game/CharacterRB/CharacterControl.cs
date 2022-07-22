05bc1386524971dac1dd21f99f715ae9e7c8c59e
Abdullah <abdullahv2730@gmail.com>
2022-07-22 19:25:58 +0300

    Initial commit

---
+++ Assets/Unity_Game/CharacterRB/CharacterControl.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace project
+{
+    public enum TransitionParameter
+    {
+        move,
+    }
+    public class CharacterControl : MonoBehaviour
+    {
+
+        [SerializeField] private float speed;
+        [SerializeField] private Animator animator;
+        void Update()
+        {
+            if (VirtualInputManager.Instance.MoveLeft && VirtualInputManager.Instance.MoveRight)
+            {
+                animator.SetBool(TransitionParameter.move.ToString(), false);
+                return;
+            }
+            if (!VirtualInputManager.Instance.MoveLeft && !VirtualInputManager.Instance.MoveRight)
+            {
+                animator.SetBool(TransitionParameter.move.ToString(), false);
+                return;
+            }
+            if (VirtualInputManager.Instance.MoveLeft)
+            {
+                this.gameObject.transform.Translate(Vector3.forward * speed * Time.deltaTime);
+                this.gameObject.transform.rotation = Quaternion.Euler(0, 180f, 0);
+                animator.SetBool(TransitionParameter.move.ToString(), true);
+            }
+
+            if (VirtualInputManager.Instance.MoveRight)
+            {
+                this.gameObject.transform.Translate(Vector3.forward * speed * Time.deltaTime);
+                this.gameObject.transform.rotation = Quaternion.Euler(0, 0, 0);
+                animator.SetBool(TransitionParameter.move.ToString(), true);
+            }
+        }
+    }
+}
+
---
+++ Assets/Unity_Game/Inputs/KeyboardInput.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace project
+{
+    public class KeyboardInput : MonoBehaviour
+    {
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if(Input.GetKey(KeyCode.A))
+            {
+                VirtualInputManager.Instance.MoveLeft = true;
+            }
+            else {
+                VirtualInputManager.Instance.MoveLeft = false;
+            }
+
+            if(Input.GetKey(KeyCode.D))
+            {
+                VirtualInputManager.Instance.MoveRight = true;
+            }
+            else {
+                VirtualInputManager.Instance.MoveRight = false;
+            }
+        }
+    }
+}
