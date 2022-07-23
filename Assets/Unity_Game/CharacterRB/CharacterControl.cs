622f0014310f1695777570faec2af2e42379c07a
Abdullah <abdullahv2730@gmail.com>
2022-07-23 15:42:47 +0300

    Add a Finite State Machine

--- Assets/Unity_Game/CharacterRB/CharacterControl.cs
+++ Assets/Unity_Game/CharacterRB/CharacterControl.cs
@@ -11,35 +11,9 @@ namespace project
     public class CharacterControl : MonoBehaviour
     {
 
-        [SerializeField] private float speed;
-        [SerializeField] private Animator animator;
-        [SerializeField] private Material material;
-        void Update()
-        {
-            if (VirtualInputManager.Instance.MoveLeft && VirtualInputManager.Instance.MoveRight)
-            {
-                animator.SetBool(TransitionParameter.move.ToString(), false);
-                return;
-            }
-            if (!VirtualInputManager.Instance.MoveLeft && !VirtualInputManager.Instance.MoveRight)
-            {
-                animator.SetBool(TransitionParameter.move.ToString(), false);
-                return;
-            }
-            if (VirtualInputManager.Instance.MoveLeft)
-            {
-                this.gameObject.transform.Translate(Vector3.forward * speed * Time.deltaTime);
-                this.gameObject.transform.rotation = Quaternion.Euler(0, 180f, 0);
-                animator.SetBool(TransitionParameter.move.ToString(), true);
-            }
-
-            if (VirtualInputManager.Instance.MoveRight)
-            {
-                this.gameObject.transform.Translate(Vector3.forward * speed * Time.deltaTime);
-                this.gameObject.transform.rotation = Quaternion.Euler(0, 0, 0);
-                animator.SetBool(TransitionParameter.move.ToString(), true);
-            }
-        }
+        public float speed;
+        public Animator animator;
+        public Material material;
 
         public void ChangeMaterial()
         {
